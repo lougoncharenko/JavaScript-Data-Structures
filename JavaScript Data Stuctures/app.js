a4393dffe4020b62ec1230bcc0f45c47c32647af
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-15 16:54:01 -0500

    Add files via upload

---
+++ JavaScript Data Stuctures/app.js
@@ -0,0 +1,36 @@
+//Use an Array to Store a Collection of Data
+
+//one-dimensional array
+let simpleArray=['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
+
+
+
+//multi-dimensional array
+let complexArray =[
+    [
+        {
+            one:1,
+            two:2
+        },
+        {
+            three:3,
+            four:4
+        }
+    ],
+    [
+        {
+            a:'a',
+            b:'b'
+        },
+        {
+            c:'c',
+            d:"d"
+        }
+    ]
+];
+console.log(complexArray.length);
+
+let yourArray =[ 
+    'this','array', 'has', 5, true, undefined
+  ];
