627d3e7abea160292a9a064a32c2f1c94fd01505
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-16 16:39:17 -0500

    Add files via upload

--- JavaScript Data Stuctures/app.js
+++ JavaScript Data Stuctures/app.js
@@ -190,4 +190,79 @@ let nestedArray = [
     // Only change code above this line
   ];
 
+
+  //Add Key-Value Pairs to JavaScript Objects
+  const tekkenCharacter = {
+    player: 'Hwoarang',
+    fightingStyle: 'Tae Kwon Doe',
+    human: true
+  };
+
+  tekkenCharacter.origin = 'South Korea';
+  tekkenCharacter['hair color'] = 'dyed orange';
+  tekkenCharacter[eyes] = 'brown';
+
+  //practice
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
   
+  // Only change code below this line
+  foods.bananas=13;
+  foods.grapes=35;
+  foods.strawberries=27;
+  // Only change code above this line
+  
+  console.log(foods);
+
+
+  //Modify an Object Nested Within an Object
+ let nestedObject = {
+    id: 28802695164,
+    date: 'December 31, 2016',
+    data: {
+      totalUsers: 99,
+      online: 80,
+      onlineStatus: {
+        active: 67,
+        away: 13,
+        busy: 8
+      }
+    }
+  };
+
+
+  let userActivity={
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data:{
+        totalUsers: 51,
+        online:42
+    }
+  }
+
+  userActivity.data.online=45;
+  console.log(userActivity);
+
+  //Access Property Names with Bracket Notation
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  function checkInventory(scannedItem) {
+    // Only change code below this line
+  return foods[scannedItem]
+    // Only change code above this line
+  }
+  
+  console.log(checkInventory("apples"));
+
+
+
