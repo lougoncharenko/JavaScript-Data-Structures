9042eb4a7555b925d7b44e9441f337621521930c
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-15 22:54:21 -0500

    Add files via upload

--- JavaScript Data Stuctures/app.js
+++ JavaScript Data Stuctures/app.js
@@ -34,3 +34,76 @@ console.log(complexArray.length);
 let yourArray =[ 
     'this','array', 'has', 5, true, undefined
   ];
+
+
+  //accessing an array's contents using bracket notation
+  let ourArray = ["a", "b", "c"];
+  console.log(ourArray[1])
+
+  let myArray = ["a", "b", "c", "d"];
+  myArray[1]="bee"
+console.log(myArray);
+
+
+//Add Items to an Array with push() and unshift()
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX');
+console.log(romanNumerals) //romanNumerals would have the value ['XIX', 'XX', 'XXI', 'XXII'].
+
+romanNumerals.push(twentyThree); //romanNumerals would have the value ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+  arr.unshift('I', 2, 'three')
+  arr.push(7, 'VIII', 9)
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+
+
+ // Remove Items from an Array with pop() and shift()
+ let greetings = ['whats up?', 'hello', 'see ya!'];
+
+greetings.pop(); // greetings would have the value ['whats up?', 'hello'].
+greetings.shift() //greetings would have the value ['hello'].
+
+function popShift(arr) {
+    let popped= arr.pop()
+    let shifted=arr.shift()
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+  //Remove Items Using splice()
+  let array = ['today', 'was', 'not', 'so', 'great'];
+
+array.splice(2, 2); //array would have the value ['today', 'was', 'great'].
+
+
+//Add Items Using splice()
+function htmlColorNames(arr) {
+    // Only change code below this line
+  const startIndex=0;
+  const amountToDelete=2;
+  arr.splice(startIndex, amountToDelete,'DarkSalmon', 'BlanchedAlmond')
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+  //Copy Array Items Using slice()
+  let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3); //todaysWeather would have the value ['snow', 'sleet']
+
+
+//Copy an Array with the Spread Operator
